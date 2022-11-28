9e9021e7a54bdd5ec0293e76d145d056bb11eb94
Tora <109001487+salamovav@users.noreply.github.com>
2022-11-28 18:26:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+//Первое задание
+
+// let a = +prompt("Угадайте загаданное число");
+// let b=1;
+
+// switch (true) {
+//     case a==b :
+//         alert('Верно');
+//         break;
+
+//     default:
+//         alert('Не верно');
+//         break;
+// }
+
+//Второе задание
+
+// let a= +prompt('Введите число от 1 до 12')
+
+// switch (a) {
+//     case 1:
+//     case 2:
+//     case 3:
+//         alert('Зима')
+//         break;
+//     case 4:
+//     case 5:
+//     case 6:
+//         alert('Весна')
+//         break;
+//     case 7:
+//     case 8:
+//     case 9:
+//         alert('Лето')
+//         break;
+//     case 10:
+//     case 11:
+//     case 12:
+//         alert('Осень')
+//         break;
+//     default:
+//         alert('Ваше число не выполняет условие вопроса!')
+//         break;
+// }
+
+//Третье задание
+
+let a=null;
+let b=3;
+let c=4;
+alert(a??b??c);
